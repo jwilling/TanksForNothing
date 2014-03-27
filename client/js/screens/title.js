c380da91521366d0cd49294186a91546ba88896b
Jonathan Willing <jwilling@me.com>
2014-03-26 20:17:44 -0500

    rewrite the title screen to use sprites

--- client/js/screens/title.js
+++ client/js/screens/title.js
@@ -1,51 +1,37 @@
 game.TitleScreen = me.ScreenObject.extend({
-	
-	// redefine the constructor
 	init : function() {
-		this.parent(true);
-
-		// title screen image
-		this.title = null;
+		// Call the parent initializer using the following parameters:
+		//		addAsObject = true
+		//		persist = false;
+		this.parent(true, false);
+		this.z = 0;
 		
-		//Any key press causes game to proceed to menu.
-		window.addEventListener('keydown', function(e){
-			window.removeEventListener('keydown', function(e){}, false);
-			me.state.change(me.state.PLAY);
-		}, false);
-	},
-	/**	
-	 *  action to perform on state change
-	 */
-	onResetEvent : function() {
-		if (this.title == null) {
-			// init stuff if not yet done
-			this.title = me.loader.getImage("Start_Menu");
-			
-			// add the object at pos (10,10), z index 4
-			me.game.add((new myButton(10,10)),0);				
+		// Set up a new sprite for the background image and
+		// add it to the game object.
+		this.background = new ImageSprite("Start_Menu");
+		me.game.world.addChild(this.background);
+		
+		// Set up an event listener to jump to the menu screen
+		// whenever a key is pressed.
+		var keyPressedHandler = function(event) {
+			// Change the state to the main menu.
+			me.state.change(STATE_MAIN_MENU);
+			window.removeEventListener('keydown', keyPressedHandler);
 		}
 		
-		me.input.bindKey(me.input.KEY.LEFT, "left");
+		// Add the event listener.
+		window.addEventListener('keydown', keyPressedHandler);
 	},
 	
-	update : function() {
-		
-		if (me.input.isKeyPressed('left')) {
-			me.state.change(me.state.PLAY);
-		}
-		return true;
+	// Called when the screen is about to be destroyed.
+	onDestroyEvent : function() {
+		// Remove the background image sprite.
+		me.game.world.removeChild(this.background);
 	},
 	
-	// the main drawing function
+	// Override the canvas drawing.
 	draw : function(context) {
-		context.drawImage(this.title, 0,0);
-	},
-	
-	/**	
-	 *  action to perform when leaving this screen (state change)
-	 */
-	onDestroyEvent: function() {
-	  me.input.unbindKey(me.input.KEY.LEFT);
-	  
-	}
+		// Clear the canvas.
+		me.video.clearSurface(context, "#000");
+	 },
 });
