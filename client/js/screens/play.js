c41494d499e5d4dd368cc2f085b12d6897a18857
Jonathan Willing <jwilling@me.com>
2014-03-27 15:38:14 -0500

    set up other player's tanks and attempt to move them
    Move when the environment is updated.

--- client/js/screens/play.js
+++ client/js/screens/play.js
@@ -1,17 +1,55 @@
+var GameIsInSession = false;
+
 game.PlayScreen = CustomScreen.extend({
 	onResetEvent: function() {
-		
+		GameIsInSession = true;
+		console.log("hey");
 		//load level
 		me.levelDirector.loadLevel("bloodGultch");
 		
 		// reset the score
 		game.data.score = 0;
 		
+		// Create the tanks.
+		this.createTanks();
+		
 		this.tank = new TankSprite(30, 30);
 		this.tank.positionChangedHandler = function(x, y) {
-			console.log("moved: " + x + ", " + y);
+			//console.log("moved: " + x + ", " + y);
 		}
 		me.game.world.addChild(this.tank);
+		
+		gameEnvUpdateCallback = updateEnvironment;
+	},
+	
+	createTanks : function() {
+		this.tanks = {};
+		
+		// Iterate over the players in the game environment (client.js).
+		for (var playerID in gameEnv.players) {
+			var player = gameEnv.players[playerID];
+			
+			// Create a new tank, associate it with the player, and
+			// add it to the world.
+			var tank = new TankSprite(player.locX, player.locY);
+			this.tanks[playerID] = tank;
+			me.game.world.addChild(tank);
+		}
+		
+		// Store our tank.
+		this.tank = tanks[myPlayerID];
+	},
+	
+	updateEnvironment : function(gameEnv) {
+		// Update the position of any tanks that aren't our own.
+		// See client.js for gameEnv.
+		for (var playerID in this.tanks) {
+			if (playerID == myPlayerID) continue;
+			
+			var player = gameEnv.players[playerID];
+			var tank = this.tanks[playerID];
+			tank.moveToPoint(player.locX, player.locY);
+		}		
 	},
 	
 	update : function() {
