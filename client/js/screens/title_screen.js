183f908068b7d6a772d42bca6a16187a62f4dbbe
Jonathan Willing <jwilling@me.com>
2014-04-03 14:30:37 -0500

    add a background in the title screen

--- client/js/screens/title_screen.js
+++ client/js/screens/title_screen.js
@@ -2,10 +2,16 @@
 	var TitleScreen = tfn.Screen.inheritWith(function(base, baseConstructor) {
 		return {
 			constructor: function() {
-				var circle = new createjs.Shape();
-				circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+				// Add the background.
+				var backgroundImage = tfn.preloader.getResult("title-background");
+				var background = new createjs.Bitmap(backgroundImage);
 				
-				this.addChild(circle);
+				this.addChild(background);
+
+				// Add an event listener for a click on the image.
+				this.addEventListener("click", function(event) {
+					alert("clicked");
+				});
 			}
 		}
 	});
