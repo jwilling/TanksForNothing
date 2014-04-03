07863af16eddbe46281865dbb275b050d5b3abc8
Jonathan Willing <jwilling@me.com>
2014-04-03 14:30:13 -0500

    add a destroy handler for the screen

--- client/js/screens/screen.js
+++ client/js/screens/screen.js
@@ -7,8 +7,15 @@
 	Screen.prototype.container_initialize = prototype.initialize;
 	Screen.prototype.initialize = function() {
 		this.container_initialize();
+		
+		// Set up an empty handler that the game will call when it transitions
+		// away from the current screen. 
+		this.onDestroyHandler = function() {
+			
+		}
+		
 		Function.initMixins(this);
-	}	
+	}
 	
 	tfn.Screen = Screen;
 })();
