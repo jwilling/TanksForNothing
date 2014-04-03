d900be44dbf4af9c0c0bad38bf28bbf16c9f9315
Jonathan Willing <jwilling@me.com>
2014-04-03 18:23:02 -0500

    who am I kidding?

--- client/js/screens/main_menu_screen.js
+++ client/js/screens/main_menu_screen.js
@@ -1,13 +1,9 @@
-(function() {
-	var MainMenuScreen = tfn.Screen.fastClass(function(base, baseConstructor) {
-		this.constructor = function() {
-			// Add the background.
-			var backgroundImage = tfn.preloader.getResult("background");
-			var background = new createjs.Bitmap(backgroundImage);
-			
-			this.addChild(background);
-		}
-	});
-	
-	tfn.MainMenuScreen = MainMenuScreen;
-})();
+tfn.MainMenuScreen = tfn.Screen.fastClass(function(base, baseConstructor) {
+	this.constructor = function() {
+		// Add the background.
+		var backgroundImage = tfn.preloader.getResult("background");
+		var background = new createjs.Bitmap(backgroundImage);
+		
+		this.addChild(background);
+	}
+});
--- client/js/screens/title_screen.js
+++ client/js/screens/title_screen.js
@@ -1,18 +1,14 @@
-(function() {
-	var TitleScreen = tfn.Screen.fastClass(function(base, baseConstructor) {
-		this.constructor = function() {
-			// Add the background.
-			var backgroundImage = tfn.preloader.getResult("title-background");
-			var background = new createjs.Bitmap(backgroundImage);
-			
-			this.addChild(background);
-	
-			// Add an event listener for a click on the image.
-			this.addEventListener("click", function(event) {
-				game.setScreenState(STATE_MAIN_MENU);
-			});
-		}
-	});
-	
-	tfn.TitleScreen = TitleScreen;
-})();
+tfn.TitleScreen = tfn.Screen.fastClass(function(base, baseConstructor) {
+	this.constructor = function() {
+		// Add the background.
+		var backgroundImage = tfn.preloader.getResult("title-background");
+		var background = new createjs.Bitmap(backgroundImage);
+		
+		this.addChild(background);
+		
+		// Add an event listener for a click on the image.
+		this.addEventListener("click", function(event) {
+			game.setScreenState(STATE_MAIN_MENU);
+		});
+	}
+});
