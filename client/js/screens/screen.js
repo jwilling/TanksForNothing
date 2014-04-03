2e5eeaa4c35040778623ef66e4ed3cc2daf06a9b
Jonathan Willing <jwilling@me.com>
2014-04-03 18:08:57 -0500

    rework the screen object to have better prototypes

--- client/js/screens/screen.js
+++ client/js/screens/screen.js
@@ -5,15 +5,21 @@
 	var prototype = new createjs.Container();
 	Screen.prototype = prototype;
 	Screen.prototype.container_initialize = prototype.initialize;
+	
+	// Set up a default destroy handler function which should be called
+	// by the object that adds and remove this object on the screen.
+	Screen.prototype.onDestroyHandler = function() {
+		// By default we want to remove all of our children when we are
+		// destroyed.
+		this.removeAllChildren();
+	}
+	
+	// Set up the prototype initialize function called whenever the
+	// screen object is created.
 	Screen.prototype.initialize = function() {
 		this.container_initialize();
 		
-		// Set up an empty handler that the game will call when it transitions
-		// away from the current screen. 
-		this.onDestroyHandler = function() {
-			
-		}
-		
+		// Initialize the mixins so subclasses can inherit from us.
 		Function.initMixins(this);
 	}
 	
