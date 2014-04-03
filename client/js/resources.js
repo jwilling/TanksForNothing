82133abcbe16a814f742af08a1989c4de7c4cc76
Jonathan Willing <jwilling@me.com>
2014-04-03 14:29:57 -0500

    add a preloading resources manifest

---
+++ client/js/resources.js
@@ -0,0 +1,8 @@
+tfn.resources = [
+	{ id: "background", src: "data/img/sliced/background.png" },
+	{ id: "title-background", src: "data/img/sliced/title-background.png" },
+	{ id: "menu-button-credits", src: "data/img/sliced/menu-button-credits.png" },
+	{ id: "menu-button-new-game", src: "data/img/sliced/menu-button-new-game.png" },
+	{ id: "menu-button-options", src: "data/img/sliced/menu-button-options.png" },
+	{ id: "menu-button-tutorial", src: "data/img/sliced/menu-button-tutorial.png" },
+];
