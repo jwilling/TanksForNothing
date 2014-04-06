5ff5eff9dd5778bc2ef5b80b97a2d5830c739b10
Steven Manning <manning@tamu.edu>
2014-04-05 22:38:24 -0500

    Added server actions to button callbacks

--- client/js/screens/join_prefs_screen.js
+++ client/js/screens/join_prefs_screen.js
@@ -1,4 +1,5 @@
 tfn.JoinPrefsScreen = tfn.Screen.fastClass(function(base, baseConstructor) {
+	
 	this.constructor = function() {
 		baseConstructor.call(this);
 		
@@ -7,12 +8,21 @@ tfn.JoinPrefsScreen = tfn.Screen.fastClass(function(base, baseConstructor) {
 		
 		// Add the create game button.
 		this.addButton("menu-button-create-game", 399, 290, function() {
-			game.setScreenState(STATE_LOBBY_HOST);
+			clientHostGame(function (data) {
+				session = data;
+				removeSessionUpdateCallback();
+				game.setScreenState(STATE_LOBBY_HOST);
+			});
+			//game.setScreenState(STATE_LOBBY_HOST);
 		});
 		
 		// Add the join game button.
 		this.addButton("menu-button-join-game", 399, 380, function() {
-			game.setScreenState(STATE_LOBBY_JOIN);
+			clientJoinGame(function (data) {
+				session = data;
+				removeSessionUpdateCallback();
+				game.setScreenState(STATE_LOBBY_JOIN);
+			});
 		});
 	}
 });
