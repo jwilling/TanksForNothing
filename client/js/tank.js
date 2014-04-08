e75751fd5dcd5f91b38d5bcf3108aa42a444d4c5
Jonathan Willing <jwilling@me.com>
2014-04-07 19:15:52 -0500

    rework tank to just use a velocity approximation

--- client/js/tank.js
+++ client/js/tank.js
@@ -4,29 +4,34 @@ tfn.Tank = tfn.PhysicalBitmap.fastClass(function(base, baseConstructor) {
 		var maximumVelocity = 300;
 		var frictionCoefficient = 0;
 		var accelerationMagnitude = 500;
-
+		
 		baseConstructor.call(this, imageName, x, y, maximumVelocity, frictionCoefficient, accelerationMagnitude);
 		
 		this.setAnchorPoint(0.5, 0.5);
 	}
 	
-	this.accelerate = function(shouldAccelerate) {		
-		if (shouldAccelerate) {
-			var radians = this.rotation * (Math.PI / 180);
-			var x = this.accelerationMagnitude * (Math.cos(radians));
-			var y = this.accelerationMagnitude * (Math.sin(radians));
+	this.accelerate = function(shouldAccelerate) {
+		// Just apply a constant velocity relative to the timestep.
+		var velocity = (shouldAccelerate ? 200 : 0);
+		var timestep = this.performTimestep();
 
-			this.setAcceleration(x, y);
-		} else {
-			this.setAcceleration(0, 0);
-		}
+		var radians = this.rotation * (Math.PI / 180);
+		var x = this.currentPosition.x + velocity * Math.cos(radians) * timestep;
+		var y = this.currentPosition.y + velocity * Math.sin(radians) * timestep;
+		
+		this.setPosition(x, y);
+	}
+
+	this.tick = function() {
+		// We don't want to inherit any simulations from
+		// the parent, so override this and keep it empty.
 	}
 	
 	this.rotateRight = function() {
-		this.rotation += 3;
+		this.rotation += this.lastTimestep * 200;
 	}
 	
 	this.rotateLeft = function() {
-		this.rotation -= 3;
+		this.rotation -= this.lastTimestep * 200;
 	}
 });
