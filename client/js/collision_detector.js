9938f3bf2f15777ebe236e12dd155d76eed32d04
Jonathan Willing <jwilling@me.com>
2014-04-19 22:30:02 -0500

    re-implement the collision detector algorithm

--- client/js/collision_detector.js
+++ client/js/collision_detector.js
@@ -5,36 +5,13 @@
 		this.y = y || 0;
 	}
 	
-	// Perform a collision test with the specified Rect, bounding map,
-	// and identifier. The identifier should be unique between bounding maps, as
-	// the map itself is cached internally.
-	var CollisionDetector = function(rect, boundingMap, identifier, velocity) {
+	// Set up a new collision tester with the specified map (bitmap)
+	// and identifier. The identifier should be unique between bounding 
+	// maps, as the map itself is cached internally.
+	var CollisionDetector = function(boundingMap, identifier) {
 		// Store the identifier and bitmap.
 		this.identifier = identifier;
 		this.bitmap = boundingMap;
-		
-		this.collisions = {
-			LEFT : false,
-			RIGHT : false,
-			TOP : false,
-			BOTTOM : false,
-			COLLISION : false // whether any collisions occurred
-		}
-		
-		// Get the relative anchor point from the object.
-		//
-		// This will be transposted into a global point later. 
-		var relativeAnchorPoint = new Point(rect.anchorPoint.x * rect.width, rect.anchorPoint.y * rect.height);
-		
-		// Get the collision rect from the object.
-		var collisionRect = this.getCollisionRect(rect, relativeAnchorPoint);
-		
-		// Calculate the rotated rect derived from individual rotation
-		// of the four points.
-		var rotatedRect = this.createRotatedRect(collisionRect, relativeAnchorPoint, rect.rotation);
-
-		// Finally actually check for collisions.
-		this.calculateCollisions(rotatedRect, velocity);
 	}
 	
 	// Convenience function to clone a point.
@@ -56,6 +33,18 @@
 		this.p2 = p2 || new Point();
 		this.p3 = p3 || new Point();
 		this.p4 = p4 || new Point();
+	}
+	
+	CollisionDetector.prototype.getTranslation = function(proposedRect, previousRect) {
+		return new tfn.Vector2D(proposedRect.x - previousRect.x, proposedRect.y - previousRect.y);
+	}
+	
+	CollisionDetector.prototype.getTranslatedRect = function(rect, translationX, translationY) {
+		// function Rect(x, y, width, height, rotation, anchorPoint);
+		var translatedRect = new tfn.Rect(rect.x, rect.y, rect.width, rect.height, rect.rotation, rect.anchorPoint);
+		translatedRect.x += translationX;
+		translatedRect.y += translationY;
+		return translatedRect;
 	}
 	
 	CollisionDetector.prototype.getCollisionRect = function(collisionRect, relativeAnchorPoint) {
@@ -188,7 +177,7 @@
 	// the edge points.
 	//
 	// Implementation taken from http://rosettacode.org/wiki/Bitmap/Bresenham's_line_algorithm
-	CollisionDetector.prototype.bline = function(x0, y0, x1, y1) {
+	CollisionDetector.prototype.bline = function(x0, y0, x1, y1, resolution) {
 		var p = [];
 		
 		x0 = Math.round(x0);
@@ -200,14 +189,18 @@
 		var dy = Math.abs(y1 - y0), sy = y0 < y1 ? 1 : -1; 
 		var err = (dx > dy ? dx : -dy) / 2;
 					
+		var iteration = 0;
 		while (true) {
-			p.push(new Point(x0, y0));
+			if (iteration % resolution == 0) {
+				p.push(new Point(x0, y0));
+			}
 			
 			if (x0 === x1 && y0 === y1) break;
 			var e2 = err;
 			
 			if (e2 > -dx) { err -= dy; x0 += sx; }
 			if (e2 < dy) { err += dx; y0 += sy; }
+			iteration++;
 		}
 		
 		return p;
@@ -217,11 +210,13 @@
 	//
 	// The array contains not only the four corner points, but
 	// additional points that are interpolated.
-	CollisionDetector.prototype.createIntermediatePoints = function(rotatedRect) {	
-		var p12Interpolations = this.bline(rotatedRect.p1.x, rotatedRect.p1.y, rotatedRect.p2.x, rotatedRect.p2.y);
-		var p23Interpolations = this.bline(rotatedRect.p2.x, rotatedRect.p2.y, rotatedRect.p3.x, rotatedRect.p3.y);
-		var p34Interpolations = this.bline(rotatedRect.p3.x, rotatedRect.p3.y, rotatedRect.p4.x, rotatedRect.p4.y);
-		var p41Interpolations = this.bline(rotatedRect.p4.x, rotatedRect.p4.y, rotatedRect.p1.x, rotatedRect.p1.y);
+	CollisionDetector.prototype.createIntermediatePoints = function(rotatedRect) {
+		var resolution = 5;
+		
+		var p12Interpolations = this.bline(rotatedRect.p1.x, rotatedRect.p1.y, rotatedRect.p2.x, rotatedRect.p2.y, resolution);
+		var p23Interpolations = this.bline(rotatedRect.p2.x, rotatedRect.p2.y, rotatedRect.p3.x, rotatedRect.p3.y, resolution);
+		var p34Interpolations = this.bline(rotatedRect.p3.x, rotatedRect.p3.y, rotatedRect.p4.x, rotatedRect.p4.y, resolution);
+		var p41Interpolations = this.bline(rotatedRect.p4.x, rotatedRect.p4.y, rotatedRect.p1.x, rotatedRect.p1.y, resolution);
 		
 		return p12Interpolations.concat(p23Interpolations).concat(p34Interpolations).concat(p41Interpolations);
 	}
@@ -234,22 +229,120 @@
 		return point;
 	}
 	
+	CollisionDetector.prototype.checkCollision = function(rect, relativeAnchorPoint) {		
+		// Derive the collision rect from the regular rect.
+		var collisionRect = this.getCollisionRect(rect, relativeAnchorPoint);
+		
+		// Calculate the rotated rects derived from individual rotation
+		// of the four points.
+		var rotatedRect = this.createRotatedRect(collisionRect, relativeAnchorPoint, rect.rotation);
+		
+		// Create a list of points that are created via interpolation
+		// between the points on the rotated rect.
+		var edgePoints = this.createIntermediatePoints(rotatedRect);
+
+		// The point at which there is a collision, if any.
+		var collisionPoint = null;
+		
+		// Iterate over all of the points and check the pixel
+		// data at that point to see if we have a collision.
+		var canvasWidth = this.cachedSamplingCanvas().width;
+		for (var i = 0; i < edgePoints.length; i++) {
+			var point = edgePoints[i];
+			var imageDataIndex = (point.y * canvasWidth + point.x) * 4;
+			
+			// Check the alpha channel. If it is not 0% opaque,
+			// we have a hit.
+			//
+			// Remember that the first three channels are RGB, 
+			// and the 4th is alpha. This means index + 3 is
+			// the alpha channel.
+			//
+			// TODO: alpha channel not working properly? Checking
+			// blue for now.
+			if (pixelDataCache[this.identifier][imageDataIndex + 3]) {
+				collisionPoint = point;
+				break;
+			}
+		}
+		
+		return collisionPoint;
+	}
 	
-	CollisionDetector.prototype.calculateCollisions = function(rotatedRect, velocity) {		
-		// We need a way to figure out the points that are closest
-		// to each edge, whether it be top, bottom, left, or right.
+	CollisionDetector.prototype.determineOpenRect = function(rectProposed, rectPrevious) {
+		// Get the relative anchor point from the object.
 		//
-		// So we create sorting functions that can sort our points
-		// from least to greatest.
+		// This will be transposted into a global point later. 
+		var r = rectPrevious;
+		var relativeAnchorPoint = new Point(r.anchorPoint.x * r.width, r.anchorPoint.y * r.height);
+		
+		// The translation is given from the previous rect, and the
+		// proposed rect is given as guidance.
+		var translation = this.getTranslation(rectProposed, rectPrevious);
+		
+		// First check proposed rect as the fast path.
+		if (!this.checkCollision(rectProposed, relativeAnchorPoint)) {
+			// No collision for the proposed rect, so let it happen.
+			return rectProposed;
+		}
+
+		// So we now know the proposed rect isn't going to be availiable.
 		//
-		// First up is a way to sort by the X values.
+		// The next step is to figure out which axis (if any) has a safe
+		// movement path available by manipulating the translation.
+		//
+		// First check to see if we are able to move only on the y-axis.
+		var yTranslatedRect = this.getTranslatedRect(rectPrevious, 0, translation.y);
+		if (!this.checkCollision(yTranslatedRect, relativeAnchorPoint)) {
+			// We can move along the y-axis safely, so return
+			// the modified rect.
+			return yTranslatedRect;
+		}
+		
+		// Our last check is to see whether we can move along the
+		// x-axis.
+		var xTranslatedRect = this.getTranslatedRect(rectPrevious, translation.x, 0);
+		if (!this.checkCollision(xTranslatedRect, relativeAnchorPoint)) {
+			return xTranslatedRect;
+		}
+		
+		// We can't move anywhere safely, so maintain the previous rect.
+		return rectPrevious;
+	}
+	
+	CollisionDetector.prototype.collisionEdge = {
+		TOP : 0,
+		BOTTOM: 1,
+		LEFT : 2,
+		RIGHT : 3,
+		NONE : 4
+	}
+	
+	CollisionDetector.prototype.determineCollisionEdge = function(currentRect) {
+		// Get the relative anchor point.
+		var r = currentRect;
+		var relativeAnchorPoint = new Point(r.anchorPoint.x * r.width, r.anchorPoint.y * r.height);
+		
+		// Determine the point of collision.
+		var collisionPoint = this.checkCollision(currentRect, relativeAnchorPoint);
+		
+		// If no collision, early bail (fast path).
+		if (!collisionPoint) {
+			return this.collisionEdge.NONE;
+		}		
+		
+		// Calculate the rotated rect.
+		var collisionRect = this.getCollisionRect(currentRect, relativeAnchorPoint);
+ 		var rotatedRect = this.createRotatedRect(collisionRect, relativeAnchorPoint, currentRect.rotation);		
+		
+		// Define a sorter for X values, from least to greatest.
 		function sortByX(p1, p2) {
 			if (p1.x < p2.x) return -1;
 			if (p1.x > p2.x) return 1;
 			return 0;
 		}
 		
-		// Then a sorter for Y values.
+		// Then a sorter for Y values, from least to greatest.
 		function sortByY(p1, p2) {
 			if (p1.y < p2.y) return -1;
 			if (p1.y > p2.y) return 1;
@@ -264,61 +357,25 @@
 		sortedByX.sort(sortByX);
 		sortedByY.sort(sortByY);
 		
-		// Create a list of points that are created via interpolation
-		// between the points on the rotated rect.
-		var edgePoints = this.createIntermediatePoints(rotatedRect);
+		// Calculate the midpoint of the rect.
+		var midpoint = new Point();
+		midpoint.x = sortedByX[0].x + (sortedByX[3].x - sortedByX[0].x) / 2;
+		midpoint.y = sortedByY[0].y + (sortedByY[3].y - sortedByY[0].y) / 2;
 		
-		// The point at which there is a collision, if any.
-		var collisionPoint = null;
+		var thresholdX = (sortedByX[3].x - sortedByX[0].x) / 4;
+		var thresholdY = (sortedByY[3].y - sortedByY[0].y) / 4;
 		
-		// The center point of the rotated rect.
-		var centerPoint = this.calculateCenterPoint(sortedByX[0].x, sortedByY[0].y, sortedByX[3].x, sortedByY[3].y);
-		
-		// Iterate over all of the points and check the pixel
-		// data at that point to see if we have a collision.
-		var canvasWidth = this.cachedSamplingCanvas().width;
-		for (var i = 0; i < edgePoints.length; i++) {
-			var point = edgePoints[i];
-			var imageDataIndex = (point.y * canvasWidth + point.x) * 4;
-			
-			// Check the alpha channel. If it is not 0% opaque,
-			// we have a hit.
-			//
-			// Remember that the first three channels are RGB, 
-			// and the 4th is alpha. This means index + 2 is
-			// the alpha channel.
-			if (pixelDataCache[this.identifier][imageDataIndex + 2]) {
-				collisionPoint = point;
-				break;
-			}
+		if (collisionPoint.y < midpoint.y - thresholdY) {
+			return this.collisionEdge.TOP;
+		} else if (collisionPoint.y > midpoint.y + thresholdY) {
+			return this.collisionEdge.BOTTOM;
+		} else if (collisionPoint.x < midpoint.x - thresholdX) {
+			return this.collisionEdge.LEFT;
+		} else if (collisionPoint.x > midpoint.x + thresholdX) {
+			return this.collisionEdge.RIGHT;
+		} else {
+			console.log("Faulty collision. middle: " + JSON.stringify(midpoint) + " collision: " + JSON.stringify(collisionPoint));
 		}
-				
-		// No collisions!
-		if (!collisionPoint) {
-			return;
-		}
-
-		var movingUp = (velocity.y > 0);
-		var movingDown = (velocity.y < 0);
-		var movingRight = (velocity.x > 0);
-		var movingLeft = (velocity.x < 0);
-		
-		//console.log("right: " + movingRight + " left: " + movingLeft + " up: " + movingUp + " down: " + movingDown);
-		
-		if (point.y <= centerPoint.y + (centerPoint.y - sortedByY[0].y) / 2 && movingUp) {
-			this.collisions.TOP = true;			
-		} else if (point.y >= centerPoint.y + (sortedByY[3].y - centerPoint.y && movingDown) / 2) {
-			this.collisions.BOTTOM = true;
-		}
-		
-		if (point.x <= centerPoint.x + (centerPoint.x - sortedByX[0].x) / 2 && movingLeft) {
-			this.collisions.LEFT = true;
-		} else if (point.x >= centerPoint.x + (sortedByX[3].x - centerPoint.x) / 2 && movingRight) {
-			this.collisions.RIGHT = true;
-		}
-
-				
-		this.collisions.COLLISION = true;
 	}
 	
 	tfn.CollisionDetector = CollisionDetector;
